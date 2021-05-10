9854668ccc971f57308e4cddf2e3b30900b931d0
quangkhanh27 <lamquangkhanh2710@gmail.com>
2021-05-10 13:43:02 +0700

    2 commit

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=app
+FLASK_APP=webapp
 FLASK_ENV=development
--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
+# from flask import Flask
+# from .Routers.TodoRouters import todo
+# from flask_cors import CORS
+
+# app = Flask(__name__)
+# CORS(app)
+# # app.config.from_object('app.settings')
+# app.register_blueprint(todo)
+
 from flask import Flask
-from .Routers.TodoRouters import todo
 
 app = Flask(__name__)
-app.register_blueprint(todo)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
