c58cd207e5d248cf380c2051b3cd345e358313d8
quangkhanh27 <lamquangkhanh2710@gmail.com>
2021-05-10 14:08:59 +0700

    5

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,10 @@
-# from flask import Flask
-# from .Routers.TodoRouters import todo
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# CORS(app)
-# # app.config.from_object('app.settings')
-# app.register_blueprint(todo)
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return "ok"
+
+if __name__ == "__main__":
+    app.run(debug=True)
