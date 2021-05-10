ee50bc7dd92ad2d1c18ddeecc50cf9fe260c7024
quangkhanh27 <lamquangkhanh2710@gmail.com>
2021-05-10 13:52:34 +0700

    3 commit

--- app/Routers/TodoRouters.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, json
-
-todo = Blueprint('todo', __name__)
-
-# http://localhost:5000/api/login  method = GET
-
-@todo.route('/api', methods=['GET'])
-def get_all():
-    return 'ok'
